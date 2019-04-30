32ef42cb5b65a48770d13ca5ec555ed3626259b8
Mark Lancaster <mlancaster@fanreact.com>
2019-04-30 08:22:21 -0500

    init

---
+++ index.php
@@ -0,0 +1,144 @@
+
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8" />
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport' user-scalable='no'>
+  <title>Momenta Tech - Powering Punchlist Zero</title>
+  <meta name="description" content="Build trust with your customers by offering them custom tools that will solve their needs." />
+  <link href="https://fonts.googleapis.com/css?family=Exo:400,700" rel="stylesheet">
+  <link href="./index.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+  <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="http://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        
+</head>
+<body>
+  
+  <div class="cta">
+    <div class="overlay"></div>
+    <div class="inner">
+      <h1>Custom engineering tools &amp; calculators</h1>
+      <h2>Developed by Momenta Tech</h2>
+    </div>
+    <img src="https://s3.us-east-2.amazonaws.com/punchlistzero/static/punchlist-zero-momenta.png" alt="Momenta tech on Punchlist Zero" />
+  </div>
+
+  <div class="secondary">
+    <div class="container">
+      <div>
+        <i class="fab fa-node-js"></i>
+        <h3>Custom javascript</h3>
+      </div>
+      <div>
+        <i class="fas fa-server"></i>
+        <h3>Cloud-based</h3>
+      </div>
+      <div>
+        <i class="fas fa-laptop-code"></i>
+        <h3>Easily embeddable</h3>
+      </div>
+      <div>
+        <i class="fas fa-calculator"></i>
+        <h3>Free options</h3>
+      </div>
+    </div>
+  </div>
+
+  <div class="tertiary">
+    <div class="container">
+      <h2>
+        Build trust with your customers
+      </h2>
+      <p>
+        Provide quick and accurate answers to your customers needs with a custom built tool or calculator. From advanced electrical engineering to project management, Momenta Tech can develop any tool you can imagine.
+      </p>
+      <div class="options">
+        <div>
+          <span>
+            FREE
+          </span>
+          <p>
+            Choose from our large library of calculators. These can be embedded into your site.
+          </p>
+        </div>
+        <div>
+          <span>
+            $500
+          </span>
+          <p>
+            Minimum engagement for 4 hours of custom design &amp; development.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="carousel">
+    <div>
+      <a href="https://www.punchlistzero.com/eu-and-awg-cable-sizing-calculator/" target="_blank">
+        <img src="https://s3.us-east-2.amazonaws.com/punchlistzero/static/minimum-cable-size.jpg" />
+      </a>
+      <a class="caption" href="https://www.punchlistzero.com/eu-and-awg-cable-sizing-calculator/" target="_blank">Cable Sizing and Length Calculator</a>
+    </div>
+    <div>
+      <a href="https://www.punchlistzero.com/raceway-fill-calculator/" target="_blank">
+        <img src="https://s3.us-east-2.amazonaws.com/punchlistzero/static/raceway-fill-calculator.jpg" />
+      </a>
+      <a class="caption" href="https://www.punchlistzero.com/eu-and-awg-cable-sizing-calculator/" target="_blank">Conduit Fill Calculator</a>
+    </div>
+    <div>
+      <a href="https://www.punchlistzero.com/flange-pressure-rating-calculator-table/" target="_blank">
+        <img src="https://s3.us-east-2.amazonaws.com/punchlistzero/static/pipe-flange-rating-calculator.jpg" />
+      </a>
+      <a class="caption" href="https://www.punchlistzero.com/eu-and-awg-cable-sizing-calculator/" target="_blank">Pipe Flange Rating Calculator</a>
+    </div>
+    <div>
+      <a href="https://www.punchlistzero.com/control-panel-heat-dissipation-calculator/" target="_blank">
+        <img src="https://s3.us-east-2.amazonaws.com/punchlistzero/static/heat-dissipation-calculator.jpg" />
+      </a>
+      <a class="caption" href="https://www.punchlistzero.com/eu-and-awg-cable-sizing-calculator/" target="_blank">Control Panel Heat Dissipation Calculator</a>
+    </div>
+  </div>
+
+  <div class="unique">
+    <div class="container">
+      <h2>Momenta Tech Experience</h2>
+      <p><a href="https://www.linkedin.com/in/ziebro/" target="_blank">Travis Ziebro</a> has worked in the Oil and Gas industry as an engineering for over 15 years while <a href="https://www.linkedin.com/in/markmakes/" target="_blank">Mark Lancaster</a> has developed dozens of applications for a myriad of industries for over ten.</p>
+      <p>
+        Together, they have developed a dozen engineering calculators and tools that are widely used across the internet. 
+      </p>
+      <p>
+        <a href="https://www.punchlistzero.com/engineering-tools-and-calculators/" target="_blank">See our current set of tools created for Punchlist Zero</a>
+      </p>
+    </div>
+  </div>
+
+  <footer>
+    &copy; 2019 Momenta Tech &nbsp; &nbsp; | &nbsp; &nbsp; <a href="https://docs.google.com/forms/d/e/1FAIpQLSeJdg8GnPco6R6pAN0pVrUF5HO4LVJSE5OkWgGAJXnp8AseKA/viewform">Contact us</a>
+  </footer>
+
+  <a id="contact" href="https://docs.google.com/forms/d/e/1FAIpQLSeJdg8GnPco6R6pAN0pVrUF5HO4LVJSE5OkWgGAJXnp8AseKA/viewform" target="_blank">
+    Get in touch <i class="fas fa-angle-right"></i>
+  </a>
+
+  <script>
+    $(document).ready(function(){
+      $('.carousel').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 4500
+      });
+    });
+  </script>
+
+</body>
+</html>
+
+
+
+
+
