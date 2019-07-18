c6ce275bb93d547b63e885e5d132be161a6cbdc6
Mark Lancaster <mlancaster@fanreact.com>
2019-07-18 17:19:53 -0500

    url change

--- mobile-mini/index.php
+++ mobile-mini/index.php
@@ -6,7 +6,7 @@
   <title>Momenta Tech - Powering MobileMini</title>
   <meta name="description" content="Build trust with your customers by offering them custom tools that will solve their needs." />
   <link href="https://fonts.googleapis.com/css?family=Exo:400,700" rel="stylesheet">
-  <link href="./index.css" rel="stylesheet">
+  <link href="../index.css" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
