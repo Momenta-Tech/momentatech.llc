11b7948abe31fc9ee9c1b5842ff54f6a29db310b
Mark Lancaster <mlancaster@fanreact.com>
2019-07-18 17:12:07 -0500

    sample page

---
+++ mobile-mini.php
@@ -0,0 +1,30 @@
+<?php ?>
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8" />
+  <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport' user-scalable='no'>
+  <title>Momenta Tech - Powering MobileMini</title>
+  <meta name="description" content="Build trust with your customers by offering them custom tools that will solve their needs." />
+  <link href="https://fonts.googleapis.com/css?family=Exo:400,700" rel="stylesheet">
+  <link href="./index.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+  <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        
+</head>
+<body>
+  
+  <momenta-calc></momenta-calc>
+  <script type="text/javascript" src="https://markmakes-misc.s3.amazonaws.com/momentacalc/app.js"></script>
+
+</body>
+</html>
+
+
+
+
+
